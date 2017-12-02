ff8ec0b3534c05107b722b5f7a7288803b2f31e8
Jerry <jerry.tamlander@helsinki.fi>
2017-12-02 11:59:41 +0200

    fix v3

--- src/main/java/wad/AliveApplication.java
+++ src/main/java/wad/AliveApplication.java
@@ -15,7 +15,7 @@ public class AliveApplication {
     }
 
     public static String travisUrl() {
-        return "https://travis-ci.org/Zappi/wepa17/";
+        return "https://travis-ci.org/Zappi/wepa17";
     }
 
     public static String herokuUrl() {
